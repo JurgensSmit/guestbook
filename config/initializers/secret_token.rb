2cd9348d548ba020a65e5303b723b06962d4c97b
Jurgens Smit <contact@jurgenssmit.com>
2013-04-17 16:01:28 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = 'dcdda6359ba78521030e6b2fd1a550f3e418f66a69bcbb6a155ca596bb60c00737695831b30c41e103a0446f5f91df692a3b65ed573871a90d6b084c3aa29cfa'
---
+++ db/migrate/20130417045253_create_entries.rb
@@ -0,0 +1,9 @@
+class CreateEntries < ActiveRecord::Migration
+  def change
+    create_table :entries do |t|
+    	t.string :name, :null => false
+
+      t.timestamps
+    end
+  end
+end
